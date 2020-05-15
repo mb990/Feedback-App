25e71f20452ef48d50f9b36b10597e3ca70bee8c
pavle.birac <pavle.birac@quantox.com>
2020-05-15 13:27:18 +0200

    Login homepage update

--- public/js/main-js.js
+++ public/js/main-js.js
@@ -1,15 +1,4 @@
 $(document).ready(function(){
-    $(".login-btn").click(function(){
-        $(".login-text").css("color", "red");
-    });
-    // $(".e-mail").onchange(function(){
-    //     if ($(".e-mail").value == "") {
-    //         $(".e-mail").css("background-color", "#d3d4d5");
-    //     }
-    //     else{
-    //         $(".e-mail").css("background-color", "red");
-    //     }
-    // });
     $(".js-e-mail").change(function(){
             $(".e-mail").css("border-color", "#ec1940");
             $(".js-mail").toggle();
@@ -17,8 +6,5 @@ $(document).ready(function(){
     $(".js-password").change(function(){
         $(".password").css("border-color", "#ec1940");
         $(".js-pass").toggle();
-
     });
-
-
 });
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,10 +6,10 @@
     <form>
         <br><br><br>
         <label class="hidden js-mail" for="email">Email</label>
-        <input class="e-mail js-e-mail" type="email" name="email" placeholder="email" value="">
+        <input class="e-mail js-e-mail" type="email" name="email" placeholder="email" required>
         <br><br><br>
         <label class="hidden js-pass" for="password">Password</label>
-        <input class="password js-password" type="password" name="password" placeholder="password" value=""><br>
+        <input class="password js-password" type="password" name="password" placeholder="password" required><br>
         <input type="submit" class="login-btn" value="LOG IN">
     </form>
 </div>
