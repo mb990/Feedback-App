d1594fe190f17b2ac1a4c8e2416d883f515e8557
pavle.birac <pavle.birac@quantox.com>
2020-05-18 12:13:35 +0200

    Message added

--- public/js/main-js.js
+++ public/js/main-js.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    //Log in input animation
     $(".js-e-mail").change(function(){
         $(".e-mail").css("border-color", "#ec1940");
         $(".js-mail").toggle();
@@ -14,15 +15,15 @@ $(document).ready(function(){
             
         });
     });
+    //Search teammate input
     $(".js-search").before("<i class='fas js-live-search'>&#xf002;</i>");
-    // $(".js-search").change(function(){
-    //     $(".js-search").css("border-color", "#ec1940");
-    //     $(".fas").css("border-color", "#ec1940");
-    //     $(".fas").css("color", "#ec1940");
-    // })
+
+    $(".js-search").attr('spellcheck',false);
+
     var $inputs = $(".js-search");      
     $inputs.on("input", function() {
         var $filled = $inputs.filter(function() { return this.value.trim().length > 0; });
         $('.js-live-search').toggleClass('js-filled', $filled.length > 0);
     });
+
 });
