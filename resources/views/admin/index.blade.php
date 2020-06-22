5853919cbab55514bb4dc15a663120956d24a902
Spasoje Gojic <spasoje.gojic@quantox.com>
2020-06-22 13:05:26 +0200

    Added message to admin if company is deactivated

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -31,6 +31,8 @@
 
     @if(!auth()->user()->active)
         <h2 class="admin-title">Your account is temporarily deactivated</h2>
+    @elseif (!auth()->user()->company->active)
+        <h2 class="admin-title">Your company is temporarily deactivated</h2>
     @else
 
         <div class="admin">
